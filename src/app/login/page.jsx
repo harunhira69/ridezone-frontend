ca06f7f0cf58140a5112d58a45764fa092806bda
harunhira69 <harunabhi4@gmail.com>
2025-11-27 03:12:54 +0600

    fix error

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,7 +1,8 @@
 "use client";
 
 import { Suspense } from "react";
-import LoginPage from "./LoginPage";
+import LoginPage from "./loginPage";
+
 
 export default function LoginWrapper() {
   return (
